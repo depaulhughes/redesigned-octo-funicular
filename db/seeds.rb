6aac9ad8b7ffe928b386862ddc6bcb68bb90bedd
Johnny Hughes <johnnyhughes321@yahoo.com>
2017-03-16 15:44:07 +0000

    final project

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,48 +8,51 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create(title: 'Rails, Angular, Postgres, and Bootstrap',
+Product.create(title: 'Baseball',
   description:
     %{<p>
-      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
-      As a Rails developer, you care about user experience and performance,
-      but you also want simple and maintainable code. Achieve all that by
-      embracing the full stack of web development, from styling with
-      Bootstrap, building an interactive user interface with AngularJS, to
-      storing data quickly and reliably in PostgreSQL. Take a holistic view of
-      full-stack development to create usable, high-performing applications,
-      and learn to use these technologies effectively in a Ruby on Rails
-      environment.
+      The ball has a cork-and-rubber core, around which yarn is tightly
+      wrapped; the cover consists of two snugly fitted pieces of white leather
+      sewn together. The circumference is 9 to 9.25 inches (23 to 23.5 cm) and 
+      the weight between 5 and 5.25 ounces (142 and 149 grams).
       </p>},
-  image_url: 'dcbang.jpg',    
-  price: 45.00)
+  image_url: 'ball1.png',    
+  price: 12.00)
 # . . .
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create(title: 'Basketball',
   description:
     %{<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
+      A basketball is a spherical inflated ball used in basketball games.
+      Basketballs typically range in size from very small promotional items 
+      only a few inches in diameter to extra large balls nearly a foot in 
+      diameter used in training exercises.
       </p>},
-  image_url: '7apps.jpg',
-  price: 26.00)
+  image_url: 'ball2.jpg',
+  price: 15.00)
 # . . .
 
-Product.create(title: 'Ruby Performance Optimization',
+Product.create(title: 'Soccer ball',
   description:
     %{<p>
-      <em>Why Ruby Is Slow, and How to Fix It</em> 
-      You don’t have to accept slow Ruby or Rails performance. In this
-      comprehensive guide to Ruby optimization, you’ll learn how to write
-      faster Ruby code—but that’s just the beginning. See exactly what makes
-      Ruby and Rails code slow, and how to fix it. Alex Dymo will guide you
-      through perils of memory and CPU optimization, profiling, measuring,
-      performance testing, garbage collection, and tuning. You’ll find that
-      all those “hard” things aren’t so difficult after all, and your code
-      will run orders of magnitude faster.
+      A football, soccer ball, or association football ball is the ball used 
+      in the sport of association football. The name of the ball varies 
+      according to whether the sport is called "football", "soccer", or 
+      "association football". The ball's spherical shape, as well as its size,
+      weight, and material composition, are specified by Law 2 of the Laws of 
+      the Game maintained by the International Football Association Board.
       </p>},
-  image_url: 'adrpo.jpg',
-  price: 46.00)
+  image_url: 'ball3.jpg',
+  price: 10.00)
+  # . . .
+
+Product.create(title: 'Tennis ball',
+  description:
+    %{<p>
+      A tennis ball is a ball designed for the sport of tennis. Tennis balls
+      are fluorescent yellow at major sporting events, but in recreational play
+      can be virtually any color. Tennis balls are covered in a fibrous felt 
+      which modifies their aerodynamic properties, and each has a white
+      curvilinear oval covering it.
+      </p>},
+  image_url: 'ball4.png',
+  price: 5.00)
